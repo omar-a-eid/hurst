c82546fe6888793c0fb855d1fecbd3308ee85c94
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-03-13 23:44:01 +0200

    Update CSS transitions and add search functionality

--- js/main.js
+++ js/main.js
@@ -6,13 +6,13 @@ window.addEventListener('scroll', function () {
     scrolled = true;
     navbar.classList.add('scroll-down');
     navbar.style.transition =
-      'height 0.3s ease-in-out, box-shadow 0.3s ease-in-out';
+      'all 0.7s ease-in-out';
   }
   if (window.scrollY === 0 && scrolled) {
     scrolled = false;
     navbar.classList.remove('scroll-down');
     navbar.style.transition =
-      'height 0.3s ease-in-out, box-shadow 0.3s ease-in-out';
+      'all 0.7s ease-in-out';
   }
 });
 
@@ -74,3 +74,26 @@ document.addEventListener("DOMContentLoaded", function() {
   });
 });
 
+document.getElementById('search-icon').addEventListener('click', function () {
+  this.classList.add('search-hover');
+  var searchForm=document.getElementById('search-form');
+  searchForm.classList.remove('d-none');
+
+});
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape') {
+    document.getElementById('search-icon').classList.remove('search-hover');
+    document.getElementById('search-form').classList.add('d-none');
+  }
+});
+
+document.getElementById('search-input').addEventListener('focus', function () {
+  this.style.border = '1px solid #c8a165';
+  this.style.boxShadow = 'none';
+});
+
+document.querySelector('.fa-xmark').addEventListener('click', function () {
+  document.getElementById('search-icon').classList.remove('search-hover');
+  document.getElementById('search-form').classList.add('d-none');
+});
