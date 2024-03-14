7c548c536ac1667f624d7fa9fdd5d5d93e6b7c7f
Engy Ahmed Abdelaziz <engya306@gmail.com>
2024-03-15 00:00:55 +0200

    Refactor displayFav function to use Bootstrap card layout

--- js/addToFav.js
+++ js/addToFav.js
@@ -4,33 +4,31 @@ function getFavorites() {
 }
 
 function displayFav() {
-  const favoritesProd = getFavorites();
-  let result = '';
-  const favProducts = document.getElementById('favItems');
-  if (favoritesProd.length === 0) {
-    result += `<h1 class="text-center">No Favorite Items</h1>`;
-  }
-  favoritesProd.forEach((product) => {
-    result += `
-<div class="card mb-3 mt-5" style="max-width: 540px;">
-  <div class="row g-0">
-    <div class="col-md-4">
-      <img src="../images/${product.image}" class="img-fluid rounded-start" alt="...">
-    </div>
-    <div class="col-md-8">
+    const favoritesProd = getFavorites();
+    let result = '';
+    const favProducts = document.getElementById('favItems');
+    if (favoritesProd.length === 0) {
+      result += `<h1 class="text-center">No Favorite Items</h1>`;
+    }
+    favoritesProd.forEach((product) => {
+      result += `
+  <div class="col-lg-4 col-md-6 mb-4">
+    <div class="card h-100">
+      <img src="../images/${product.image}" class="card-img-top" alt="...">
       <div class="card-body">
         <h5 class="card-title">${product.productName}</h5>
         <p class="card-text">${product.price}</p>
-        <i class="fa-solid fa-cart-plus ms-3" aria-hidden="true"></i>
-        <i class="fa-solid fa-heart-circle-plus ms-3 addToFav" aria-hidden="true" data-id="${product.id}" onclick="addedToFavourite(${product.id})"></i>
+        <div class="d-flex justify-content-between align-items-center">
+          <i class="fa-solid fa-cart-plus" aria-hidden="true"></i>
+          <i class="fa-solid fa-heart-circle-plus addToFav" aria-hidden="true" data-id="${product.id}" onclick="addedToFavourite(${product.id})"></i>
+        </div>
       </div>
     </div>
   </div>
-</div>
-  `;
-  });
-
-  favProducts.innerHTML = result;
-}
-
+    `;
+    });
+  
+    favProducts.innerHTML = result;
+  }
+  
 document.addEventListener('DOMContentLoaded', displayFav);
