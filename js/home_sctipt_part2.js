8e5c03d269ab84198b898e30e3addb0380e11bde
Mohamed Algharabawy <mohamedalgharabawy6@gmail.com>
2024-03-23 15:38:43 +0200

    Updating in products responsive and style

--- js/home_sctipt_part2.js
+++ js/home_sctipt_part2.js
@@ -75,7 +75,7 @@ function displayProducts(products) {
     }
 
     let productHTML = `
-        <div class="col-6 col-sm-6 col-md-4 col-lg-3 mb-3 card-parent">
+        <div class="col-8 col-sm-8 col-md-4 col-lg-3 mb-3 card-parent">
             <div class="h-100 card-content p-2">
                 <div class="card border-0 w-100 h-100">
                     <img src="./images/${product.image}" alt="${
